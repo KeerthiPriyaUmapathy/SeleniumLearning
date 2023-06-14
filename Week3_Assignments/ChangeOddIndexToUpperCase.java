e62bf1e1a6b8bb34ae1f923d11929df793688e14
KeerthiPriyaUmapathy <134957357+KeerthiPriyaUmapathy@users.noreply.github.com>
2023-06-14 22:21:21 +0530

    Update ChangeOddIndexToUpperCase.java

--- Week3_Assignments/ChangeOddIndexToUpperCase.java
+++ Week3_Assignments/ChangeOddIndexToUpperCase.java
@@ -9,11 +9,13 @@ public class ChangeOddIndexToUpperCase {
 		System.out.println("Input string = "+str);	
 				System.out.print("ChangeOddIndexToUpperCase string = ");
 	
-				for(int i=0; i<chArray.length+1; i++)
+				for(int i=0; i<chArray.length; i++)
 				{
 					if(i%2!=0) {
 						System.out.print(Character.toUpperCase(chArray[i]));
 					}
+					else
+						System.out.print(chArray[i]);
 					
 				}
 		
